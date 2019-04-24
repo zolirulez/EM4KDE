2bba9d505fca1f050e7609841280e554e4ceaeb6
Zoltan Mark Pinter <pinter@danfoss.com>
2019-04-24 19:43:23 +0200

    Minor correction in the plot for misseddetection/falsealarm

--- Zoltan/evaluation.m
+++ Zoltan/evaluation.m
@@ -33,19 +33,18 @@ title(['ROC for 5 degrees deviation,noisy, AUC: ' num2str(AUC)])
 
 % Finding optimal threshold
 figure(5)
-plot(T,Y./X)
+plot(T,X)
 hold on
-plot(T,X./(1-Y))
+plot(T,1-Y)
 hold off
 xlabel('probability threshold')
 % Y: TPR, X: FPR
-legend('Detection/missed detection','Missed detection/false alarm')
-% TODO
+legend('Missed detection','False alarm')
 
 % Comparison to logistic regression
 figure(6)
 logreg = fitglm(data(:,1:2),data(:,3),'Distribution','normal','Link','log');
-[X,Y,T,AUC] = perfcurve([zeros(N_eval/2,1); ones(N_eval/4,1)],...
+[X,Y,~,AUC] = perfcurve([zeros(N_eval/2,1); ones(N_eval/4,1)],...
     [ones(N_eval*3/4,1) [evaldata(1:N_eval/2,1:2); evaldata(N_eval/4*3+1:end,1:2)]]*logreg.Coefficients{:,1},1);
 plot(X,Y)
 hold on
